0858a355f12fd348b5544795ff894f6fa3fa5778
Taylor Quinn <ktquinn13@gmail.com>
2017-09-21 13:14:08 -0400

    y u always lying

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-/* affix the navbar after scroll below header */
-$('#nav').affix({
-      offset: {
-        top: $('#services').offset().top
-      }
-});	
+function start() {
+
+var elementPosition = $('#header_nav').offset();
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > elementPosition.top) {
+    $('#header_nav').css('position', 'fixed').css('top', '0');
+  } else {
+    $('#header_nav').css('position','static');
+  }
+});
+
+$(window).scroll(function(){
+    $("#header_pic").css("opacity", 1 - $(window).scrollTop() / 1550);
+}); 
+
+}
+
+window.onload = start;
