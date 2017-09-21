c2cbb672db1b14081e2ae4ddbbeada16e12e0842
Taylor Quinn <ktquinn13@gmail.com>
2017-09-20 22:40:35 -0400

    Attempt to clean up/ make prettier

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// path constants
+define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
+define('BASE_URL','https://taylorquinn.github.io');
+
+// database constants
+/*define('DB_HOST','127.0.0.1');
+define('DB_USER','root');
+define('DB_PASS','Gotjsd13!');
+define('DB_DATABASE','ktaylorquinn');
+*/
