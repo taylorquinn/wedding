b7aa50613062d0afc02f31ae5dd960e615964288
Taylor Quinn <taylorquinn@Taylors-MacBook-Air.local>
2022-04-25 15:55:35 -0400

    remove handlesubmit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,9 +7,9 @@ import "./Home.css";
 
 export const Login = (): JSX.Element => {
   let navigate = useNavigate();
-  interface stateType {
-    state: { from: { pathname: string } };
-  }
+  // interface stateType {
+  //   state: { from: { pathname: string } };
+  // }
   // let location = useLocation() as stateType;
   let auth = useAuth();
   // let from = location.state?.from?.pathname || "/";
@@ -20,26 +20,26 @@ export const Login = (): JSX.Element => {
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
+    console.log("HANDLE SUBMIT");
     let formData = new FormData(event.currentTarget);
     let username = formData.get("username") as string;
     global.console.log(username);
-    console.log(username);
+    // console.log(username);
     console.log(from);
 
-    auth.signin(username, (success: boolean) => {
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
-      if (success) {
-        //    navigate(from, { replace: true });
-      } else {
-        setSigninFailure(true);
-      }
-    });
+    // auth.signin(username, (success: boolean) => {
+    //   // Send them back to the page they tried to visit when they were
+    //   // redirected to the login page. Use { replace: true } so we don't create
+    //   // another entry in the history stack for the login page.  This means that
+    //   // when they get to the protected page and click the back button, they
+    //   // won't end up back on the login page, which is also really nice for the
+    //   // user experience.
+    //   if (success) {
+    //     //    navigate(from, { replace: true });
+    //   } else {
+    //     setSigninFailure(true);
+    //   }
+    // });
   }
 
   return (
