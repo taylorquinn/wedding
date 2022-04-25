f57c8d5a92a56bba411f9e97f7788c2d1bd52c2b
Taylor Quinn <taylorquinn@Taylors-MacBook-Air.local>
2022-04-25 14:58:34 -0400

    updated output

--- webpack.config.js
+++ webpack.config.js
@@ -201,7 +201,7 @@ module.exports = function (webpackEnv) {
     entry: paths.appIndexJs,
     output: {
       // The build folder.
-      path: path.resolve(__dirname, "dist"), // paths.appBuild,
+      path: paths.appBuild,
       // Add /* filename */ comments to generated require()s in the output.
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
