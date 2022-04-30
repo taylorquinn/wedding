7a5e05d5c8fcdb422795c03bd4d9626db0426f43
Taylor Quinn <taylorquinn@Taylors-MacBook-Air.local>
2022-04-29 21:59:15 -0400

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   // Navigate,
   Route,
   Routes,
